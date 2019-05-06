87621d9dc0c4110a56454b6f5a2c2e92378e9ab5
unknown <julietaextra14@gmail.com>
2019-05-06 18:54:01 -0300

    se actualizaron las clases

--- test/ar/edu/unlam/TestCuentaSueldo.java
+++ test/ar/edu/unlam/TestCuentaSueldo.java
@@ -10,18 +10,18 @@ public class TestCuentaSueldo {
 	public void test() {
 		
 		    CuentaSueldo cuenta = new CuentaSueldo();
-			cuenta.depositar(4000.0D);
-			Double sueldoEsperado=4000.0D;
+			cuenta.depositar(500.0D);
+			Double sueldoEsperado=500.0D;
 			Double sueldoActual= cuenta.getSaldo();
 			assertEquals(sueldoEsperado, sueldoActual);
 			
-			cuenta.extraer(500.0);
-			Double sueldoEsperado1=3500.0D;
+			cuenta.extraer(100.0);
+			Double sueldoEsperado1=400.0D;
 			Double sueldoActual1= cuenta.getSaldo();
 			assertEquals(sueldoEsperado1, sueldoActual1);
 			
 			Double sueldoEsperado2=null;
-			Double sueldoActual2= cuenta.extraer(4000.0);
+			Double sueldoActual2= cuenta.extraer(500.0);
 			assertEquals(sueldoEsperado2, sueldoActual2);
 	}
 
